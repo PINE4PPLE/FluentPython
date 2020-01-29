7bd078f7afbc9ea1e17247a3a0a33b8a1ff89c0a
PINE4PPLE <46532375+PINE4PPLE@users.noreply.github.com>
2020-01-29 13:44:54 +0800

    Add files via upload

---
+++ clip.py
@@ -0,0 +1,13 @@
+def clip(text, max_len = 80):
+    end = None
+    if len(text)> max_len:
+        space_before = text.rfind(' ', 0, max_len)
+        if space_before >= 0:
+            end = space_before
+        else:
+            space_after = text.rfind(' ', max_len)
+        if space_after >= 0:
+            end = space_after
+        if end is None:
+            end = len(text)
+        return text[:end].rstrip()
